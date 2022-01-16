344e772b4e02ec17bb22a39a9dd047885fd86def
Michael Hua <huayl.michael@gmail.com>
2022-01-15 23:12:25 -0500

    V1.0 1st complete draft

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process';
 
-const output = (fn) =>{
-  return new Promise((resolve, reject)=>{
+const output = (fn) => {
+  return new Promise((resolve, reject) => {
     fn.stdout.on('data', data => resolve(data));
   })
 }
@@ -19,14 +19,17 @@ describe("Main app function", () => {
 
   it('should output desired ouputs with valid inputs', async () => {
     const app = spawn('node', ['index.js']);
-    const input = ['1\r','Rover1\r','30 20\r','10 5 E\r','RMMMMLMM\r'];
+    const input = ['2\r', 'Rover1\r', 'Rover2\r', '30 20\r', '10 5 E\r', 'RMMMMLMM\r', '20 10 N\r', 'RMMMLMMMM\r'];
     await output(app);
-    for (let i=0; i<input.length; i++) {
+    for (let i = 0; i < input.length; i++) {
       await output(app);
       app.stdin.write(input[i]);
     }
-    const finalOutput = (await output(app)).toString();
-    expect(finalOutput).toContain('Rover1: 12 1 E'); 
+    const finalOutput1 = (await output(app)).toString();
+    const finalOutput2 = (await output(app)).toString();
+    expect(finalOutput1).toContain('Rover1: 12 1 E');
+    expect(finalOutput2).toContain('Rover2: 23 14 N');
+    app.stdin.write('q\n');
     app.kill();
   })
 })
